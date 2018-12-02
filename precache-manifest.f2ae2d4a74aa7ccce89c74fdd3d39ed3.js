05abf2c7d9d8351b1dd819ffec5764e4f50f28a4
Karla <karlavidal27@gmail.com>
2018-12-02 18:48:14 +0100

    Updates

---
+++ precache-manifest.f2ae2d4a74aa7ccce89c74fdd3d39ed3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c797ff6df3080e2f3f24b887fc8adc92",
+    "url": "/portfolio/static/media/about_wheel.c797ff6d.svg"
+  },
+  {
+    "revision": "82f73a628aacbef2e1bd",
+    "url": "/portfolio/static/css/main.79a4ae33.chunk.css"
+  },
+  {
+    "revision": "cf7abf045c7a2ff306a8",
+    "url": "/portfolio/static/js/1.cf7abf04.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "37d826c0cf6357ef6c3776ee6a0ff94f",
+    "url": "/portfolio/static/media/home_wheel.37d826c0.svg"
+  },
+  {
+    "revision": "82f73a628aacbef2e1bd",
+    "url": "/portfolio/static/js/main.82f73a62.chunk.js"
+  },
+  {
+    "revision": "5cd723b75a1682f8bca2bd7b665bffae",
+    "url": "/portfolio/static/media/projects_wheel.5cd723b7.svg"
+  },
+  {
+    "revision": "c86c5c2340a2177c6436deb67bb8355e",
+    "url": "/portfolio/static/media/medium.c86c5c23.svg"
+  },
+  {
+    "revision": "f7a728bccfa8f1f2bc8642e41edac7bb",
+    "url": "/portfolio/static/media/linked-in.f7a728bc.svg"
+  },
+  {
+    "revision": "5aeef868227fcd424b5cbf038ce7169f",
+    "url": "/portfolio/static/media/background.5aeef868.svg"
+  },
+  {
+    "revision": "4736c1d784e6ccc0d017f12c347ffca5",
+    "url": "/portfolio/index.html"
+  }
+];
